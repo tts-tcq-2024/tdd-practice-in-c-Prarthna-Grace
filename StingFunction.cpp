1a739f2709a8f2939c4ac5d91d4ad570b8b0070e
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-19 16:02:13 +0530

    Update StingFunction.cpp

--- StingFunction.cpp
+++ StingFunction.cpp
@@ -1,2 +1,14 @@
 #include "StringCalculator.h"
+#include<string.h>
+#include <ctype.h>
+
+int isEmpty(const char* input){
+  return (isdigit(*input)); {
+}
+
+void String_function(const char*input){
+  if(!(isEmpty(input))){
+      result[0]='\0';
+   }
+  
 
