c3b05b5436116bc40f38e032657bbcc6ee0dead0
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-19 16:30:33 +0530

    Update StingFunction.cpp

--- StingFunction.cpp
+++ StingFunction.cpp
@@ -2,13 +2,21 @@
 #include<string.h>
 #include <ctype.h>
 
-int isEmpty(const char* input){
-  return (isdigit(*input));
+//Function to check if the input string has numbers
+int Check_numbers(const char* input){
+  while(*input){
+    return (isdigit(*input));
+  }
+}
+// Function to check if the input string is empty
+int isEmpty(const char* input) {
+    return (input[0] == '\0');  // Return 1 if string is empty, 0 otherwise
 }
 
-int add(const char*input{
-  if(!(isEmpty(input))){
-      char result[0]='\0';
+int add(const char*input){
+  char result[];
+  if((!(Check_numbers(input))) || isEmpty(input)){
+      result[0]='\0';
    }
   return result;
 }
