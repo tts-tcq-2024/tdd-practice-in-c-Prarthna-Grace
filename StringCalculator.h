3cb2500be22bbb397e5c8be7e57ccad3831532cd
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-20 13:01:33 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -30,10 +30,14 @@ int add(const char* input){
     int len= strlen(input); 
     int returnEmpty=0;
     if (isEmpty(input) || !(Check_numbers(input)) // Return 0 if the input is empty or contains no digits
+    {
         returnEmpty=1;
+    }  
     returnEmpty &= SingleZero(input,len);
     if (returnEmpty == 1)
+    {
         return 0;
+    }
 }
 
 
