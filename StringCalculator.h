eaeac4b0db82fec2779b9c90b63c3bff045221fd
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-20 12:52:08 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -21,17 +21,16 @@ int isEmpty(const char* input) {
 }
 
 // Function to check if the input string is a single zero
-int SingleZero(const char*input, int len)
-{
+int SingleZero(const char*input, int len){
     if(len ==1 && input[0]==0)
         return 1;
 }
 
 int add(const char* input){
+    int len= strlen(input); 
     if (isEmpty(input) || !(Check_numbers(input)) || SingleZero(input,len)) {
         return 0;  // Return 0 if the input is empty or contains no digits
     }
-    int len= strlen(input); 
 }
 
 
